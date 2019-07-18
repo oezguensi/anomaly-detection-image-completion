e581851f7f8792232d69d7a98433e135abcc9040
Oezguen Sinan, FG-AM-7 <sinan.oezguen@bmwgroup.com>
2019-07-18 02:12:13 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Welcome
+Automating the detection of unwanted surface anomalies on objects such as rims. This projects implements the approach from the paper Anomaly Detection using Deep Learning based Image Completion.
---
+++ model_helpers.py
@@ -0,0 +1,153 @@
+import tensorflow as tf
+import keras.backend as K
+
+from keras.models import Model
+from keras.layers import Input, Conv2D, Lambda, UpSampling2D, ELU
+
+
+"""
+Model parameters from the original paper: https://arxiv.org/pdf/1811.06861.pdf 
+"""
+input_shape = (128, 128, 3)
+
+conv_layer_datas = [
+    {
+        'kernel_size': 5,
+        'dilation_rate': 1,
+        'strides': 1, 
+        'filters': 32
+    },
+    {
+        'kernel_size': 3,
+        'dilation_rate': 1,
+        'strides': 1, 
+        'filters': 64
+    },
+    {
+        'kernel_size': 3,
+        'dilation_rate': 1,
+        'strides': 1, 
+        'filters': 64
+    },
+    {
+        'kernel_size': 3,
+        'dilation_rate': 1,
+        'strides': 2, 
+        'filters': 128
+    },
+    {
+        'kernel_size': 3,
+        'dilation_rate': 1,
+        'strides': 1, 
+        'filters': 128
+    },
+    {
+        'kernel_size': 3,
+        'dilation_rate': 1,
+        'strides': 1, 
+        'filters': 128
+    },
+    {
+        'kernel_size': 3,
+        'dilation_rate': 2,
+        'strides': 1, 
+        'filters': 128
+    },
+    {
+        'kernel_size': 3,
+        'dilation_rate': 4,
+        'strides': 1, 
+        'filters': 128
+    },
+    {
+        'kernel_size': 3,
+        'dilation_rate': 8,
+        'strides': 1, 
+        'filters': 128
+    },
+    {
+        'kernel_size': 3,
+        'dilation_rate': 16,
+        'strides': 1, 
+        'filters': 128
+    },
+    {
+        'kernel_size': 3,
+        'dilation_rate': 1,
+        'strides': 1, 
+        'filters': 128
+    },
+    {
+        'kernel_size': 3,
+        'dilation_rate': 1,
+        'strides': 1, 
+        'filters': 128
+    },
+    {
+        'kernel_size': 3,
+        'dilation_rate': 1,
+        'strides': 1, 
+        'filters': 64
+    },
+    {
+        'kernel_size': 3,
+        'dilation_rate': 1,
+        'strides': 1, 
+        'filters': 64
+    },
+    {
+        'kernel_size': 3,
+        'dilation_rate': 1,
+        'strides': 1, 
+        'filters': 32
+    },
+    {
+        'kernel_size': 3,
+        'dilation_rate': 1,
+        'strides': 1, 
+        'filters': 16
+    },
+    {
+        'kernel_size': 3,
+        'dilation_rate': 1,
+        'strides': 1, 
+        'filters': 1
+    }
+]
+
+def create_anomaly_cnn(input_shape=input_shape, conv_layer_datas=conv_layer_datas):
+    """
+    Creates the CNN used for Anomaly Detection.
+    :param input_shape: The shape of the inputed image
+    :param conv_layer_datas: The layer parameters of the model
+    :return: Returns the model
+    """
+
+    def conv_block(outputs, kernel, dilation, strides, filters):
+        outputs = Lambda(
+            lambda x: tf.pad(x,
+                             [[0, 0],
+                              [dilation, dilation],
+                              [dilation, dilation],
+                              [0, 0]] if len(input_shape) == 3 else [[0, 0],
+                                                                     [dilation, dilation],
+                                                                     [dilation, dilation]],
+                             'REFLECT'))(outputs)
+        outputs = Conv2D(filters=filters, kernel_size=kernel, strides=strides, padding='valid', dilation_rate=dilation,
+                         activation='linear')(outputs)
+        return ELU()(outputs)
+    
+    inputs = Input(shape=input_shape)
+    outputs = inputs
+    for i, data in enumerate(conv_layer_datas):
+        outputs = conv_block(outputs, data['kernel_size'], data['dilation_rate'], data['strides'], data['filters'])
+        outputs = UpSampling2D(size=2)(outputs) if i == 11 else outputs
+    outputs = Lambda(lambda x: K.clip(x, -1, 1), name='clip')(outputs)
+    outputs = Lambda(lambda x: tf.pad(x, [[0, 0],
+                                          [1, 1],
+                                          [1, 1],
+                                          [0, 0]] if len(input_shape) == 3 else [[0, 0],
+                                                                                 [1, 1],
+                                                                                 [1, 1]],
+                                      'REFLECT'))(outputs)
+    return Model(inputs=inputs, outputs=outputs)
