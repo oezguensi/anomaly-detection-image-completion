c21cd37320c13c304831fc41d1f34a78cbd259aa
Oezguen Sinan, FG-AM-7 <sinan.oezguen@bmwgroup.com>
2019-07-18 10:48:57 -0400

    refactor creating model

--- model_helpers.py
+++ model_helpers.py
@@ -10,119 +10,36 @@ Model parameters from the original paper: https://arxiv.org/pdf/1811.06861.pdf
 """
 input_shape = (128, 128, 1)
 
-conv_layer_datas = [
-    {
-        'kernel_size': 5,
-        'dilation_rate': 1,
-        'strides': 1,
-        'filters': 32
-    },
-    {
-        'kernel_size': 3,
-        'dilation_rate': 1,
-        'strides': 1,
-        'filters': 64
-    },
-    {
-        'kernel_size': 3,
-        'dilation_rate': 1,
-        'strides': 1,
-        'filters': 64
-    },
-    {
-        'kernel_size': 3,
-        'dilation_rate': 1,
-        'strides': 2,
-        'filters': 128
-    },
-    {
-        'kernel_size': 3,
-        'dilation_rate': 1,
-        'strides': 1,
-        'filters': 128
-    },
-    {
-        'kernel_size': 3,
-        'dilation_rate': 1,
-        'strides': 1,
-        'filters': 128
-    },
-    {
-        'kernel_size': 3,
-        'dilation_rate': 2,
-        'strides': 1,
-        'filters': 128
-    },
-    {
-        'kernel_size': 3,
-        'dilation_rate': 4,
-        'strides': 1,
-        'filters': 128
-    },
-    {
-        'kernel_size': 3,
-        'dilation_rate': 8,
-        'strides': 1,
-        'filters': 128
-    },
-    {
-        'kernel_size': 3,
-        'dilation_rate': 16,
-        'strides': 1,
-        'filters': 128
-    },
-    {
-        'kernel_size': 3,
-        'dilation_rate': 1,
-        'strides': 1,
-        'filters': 128
-    },
-    {
-        'kernel_size': 3,
-        'dilation_rate': 1,
-        'strides': 1,
-        'filters': 128
-    },
-    {
-        'kernel_size': 3,
-        'dilation_rate': 1,
-        'strides': 1,
-        'filters': 64
-    },
-    {
-        'kernel_size': 3,
-        'dilation_rate': 1,
-        'strides': 1,
-        'filters': 64
-    },
-    {
-        'kernel_size': 3,
-        'dilation_rate': 1,
-        'strides': 1,
-        'filters': 32
-    },
-    {
-        'kernel_size': 3,
-        'dilation_rate': 1,
-        'strides': 1,
-        'filters': 16
-    },
-    {
-        'kernel_size': 3,
-        'dilation_rate': 1,
-        'strides': 1,
-        'filters': 1
-    }
+original_conv_layer_datas = [
+    {'kernel_size': 5, 'dilation_rate': 1, 'strides': 1, 'filters': 32},
+    {'kernel_size': 3, 'dilation_rate': 1, 'strides': 1, 'filters': 64},
+    {'kernel_size': 3, 'dilation_rate': 1, 'strides': 1, 'filters': 64},
+    {'kernel_size': 3, 'dilation_rate': 1, 'strides': 2, 'filters': 128},
+    {'kernel_size': 3, 'dilation_rate': 1, 'strides': 1, 'filters': 128},
+    {'kernel_size': 3, 'dilation_rate': 1, 'strides': 1, 'filters': 128},
+    {'kernel_size': 3, 'dilation_rate': 2, 'strides': 1, 'filters': 128},
+    {'kernel_size': 3, 'dilation_rate': 4, 'strides': 1, 'filters': 128},
+    {'kernel_size': 3, 'dilation_rate': 8, 'strides': 1, 'filters': 128},
+    {'kernel_size': 3, 'dilation_rate': 16, 'strides': 1, 'filters': 128},
+    {'kernel_size': 3, 'dilation_rate': 1, 'strides': 1, 'filters': 128},
+    {'kernel_size': 3, 'dilation_rate': 1, 'strides': 1, 'filters': 128},
+    {'kernel_size': 3, 'dilation_rate': 1, 'strides': 1, 'filters': 64},
+    {'kernel_size': 3, 'dilation_rate': 1, 'strides': 1, 'filters': 64},
+    {'kernel_size': 3, 'dilation_rate': 1, 'strides': 1, 'filters': 32},
+    {'kernel_size': 3, 'dilation_rate': 1, 'strides': 1, 'filters': 16},
+    {'kernel_size': 3, 'dilation_rate': 1, 'strides': 1, 'filters': 1}
 ]
 
 
-def create_anomaly_cnn(input_shape=input_shape, conv_layer_datas=conv_layer_datas, model_width=1):
+def create_anomaly_cnn(input_shape=input_shape, conv_layer_datas=None, model_width=1):
     """
     Creates the CNN used for Anomaly Detection.
     :param input_shape: The shape of the inputed image
     :param conv_layer_datas: The layer parameters of the model
+    :param model_width: Determines the rate to which factor the number of filter will be increased
     :return: Returns the model
     """
+    conv_layer_datas = conv_layer_datas if conv_layer_datas else original_conv_layer_datas
 
     def conv_block(outputs, kernel, dilation, strides, filters, model_width):
         outputs = Lambda(
@@ -138,7 +55,7 @@ def create_anomaly_cnn(input_shape=input_shape, conv_layer_datas=conv_layer_data
         outputs = conv_block(outputs, data['kernel_size'], data['dilation_rate'], data['strides'], data['filters'],
                              model_width if i != len(conv_layer_datas) - 1 else 1)
         outputs = UpSampling2D(size=2)(outputs) if i == 11 else outputs
-    outputs = Lambda(lambda x: K.clip(x, -1, 1), name='clip')(outputs)
     outputs = Lambda(lambda x: tf.pad(x, [[0, 0], [1, 1], [1, 1], [0, 0]], 'REFLECT'))(outputs)
+    outputs = Lambda(lambda x: K.clip(x, -1, 1), name='clip')(outputs)
 
     return Model(inputs=inputs, outputs=outputs)
